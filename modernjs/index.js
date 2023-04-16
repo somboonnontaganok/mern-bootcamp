6ed5bd1f962806f6acc89904b1b68fdd4d6f6b30
somboonnontaganok <somboonnontaganok@gmail.com>
2023-04-16 22:36:43 +0700

    let cannot access window

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,2 +1,7 @@
-// window.localStorage.setItem('test', 'This is my test...') //save to local storage
-window.localStorage.removeItem('test')
+var n = 'poon'
+n = 'zen' //window.n = zen
+// alert(n)
+
+let n1 = 'poon1'
+n1 = 'zen1' // window.n1 = undefied
+alert(n1) 
